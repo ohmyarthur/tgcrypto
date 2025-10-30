927fa681d440afb038169519bad761ab4890def3
Moeamor <ohmyarthurr@gmail.com>
2025-10-30 18:51:17 +0700

    ci: upgrade cibuildwheel and enhance cross-compilation setup
    - Update cibuildwheel to v2.21.3 and set up QEMU for Linux ARM builds
    - Implement CI-aware compilation flags for various platforms
    - Add artifact merging step for improved build outputs handling
    - Update project metadata and license information in pyproject.toml

--- setup.py
+++ setup.py
@@ -19,8 +19,10 @@
 #  Modified by ohmyarthur <https://github.com/ohmyarthur>
 
 import sys
+import os
+import platform
 from pathlib import Path
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
 
 here = Path(__file__).parent.resolve()
 
@@ -31,25 +33,53 @@ version = {}
 with open(here / "tgcrypto" / "_version.py") as f:
     exec(f.read(), version)
 
-extra_compile_args = []
-extra_link_args = []
 
-if sys.platform != 'win32':
-    extra_compile_args.extend([
-        '-O3',
-        '-march=native',
-        '-mtune=native',
-        '-ffast-math',
-        '-funroll-loops',
-        '-finline-functions',
-        '-flto',
-        '-fomit-frame-pointer',
-        '-DNDEBUG',
-    ])
-    extra_link_args.extend(['-flto'])
-else:
-    extra_compile_args.extend(['/O2', '/GL', '/DNDEBUG'])
-    extra_link_args.extend(['/LTCG'])
+def get_compile_args():
+    """Get platform-specific compiler arguments that work in CI environments."""
+    extra_compile_args = []
+    extra_link_args = []
+    
+    if sys.platform == 'win32':
+        extra_compile_args.extend(['/O2', '/GL', '/DNDEBUG'])
+        extra_link_args.extend(['/LTCG'])
+    else:
+        extra_compile_args.extend([
+            '-O3',
+            '-ffast-math',
+            '-funroll-loops',
+            '-finline-functions',
+            '-fomit-frame-pointer',
+            '-DNDEBUG',
+        ])
+        
+        if os.environ.get('CIBUILDWHEEL', '0') != '1' or os.environ.get('ENABLE_LTO', '0') == '1':
+            extra_compile_args.append('-flto')
+            extra_link_args.append('-flto')
+        
+        machine = platform.machine().lower()
+        is_ci = os.environ.get('CI', 'false').lower() == 'true' or os.environ.get('CIBUILDWHEEL', '0') == '1'
+        
+        if is_ci:
+            if sys.platform == 'darwin':
+                arch = os.environ.get('ARCHFLAGS', '')
+                if 'arm64' in arch or machine in ('arm64', 'aarch64'):
+                    extra_compile_args.extend(['-mcpu=apple-m1'])
+                elif 'x86_64' in arch or machine in ('x86_64', 'amd64'):
+                    extra_compile_args.extend(['-march=x86-64', '-mtune=generic'])
+            elif machine in ('x86_64', 'amd64'):
+                extra_compile_args.extend(['-march=x86-64', '-mtune=generic'])
+            elif machine in ('aarch64', 'arm64'):
+                extra_compile_args.extend(['-march=armv8-a'])
+        else:
+            if sys.platform == 'darwin' and machine == 'arm64':
+                extra_compile_args.extend(['-mcpu=apple-m1'])
+            else:
+                extra_compile_args.extend(['-march=native', '-mtune=native'])
+    
+    return extra_compile_args, extra_link_args
+
+
+extra_compile_args, extra_link_args = get_compile_args()
 
 setup(
     ext_modules=[
